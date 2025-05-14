ade559034c3f9519909c25fe5d4d1c4fdba80bbd
Stéphane Dresse <sdresse@bluesquarehub.com>
2025-05-14 17:37:33 +0200

    This is my second commit

--- sd_training_project/my_first_script.py
+++ sd_training_project/my_first_script.py
@@ -1,6 +1,7 @@
 print ("hi Stéphane")
 import pandas as pd
-my_list = [1, 2, 3]
+my_list = [1, 2, 3, 4, 5]
 my_list = [x + 1 for x in my_list]
 print("Now it's time to submit to GitHub")
+print("This is a new line")
 
