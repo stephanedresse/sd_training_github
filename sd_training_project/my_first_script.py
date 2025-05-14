a61b4949cfb594bf081940af69b1836f18d1be11
Stéphane Dresse <sdresse@bluesquarehub.com>
2025-05-14 17:48:18 +0200

    test

--- sd_training_project/my_first_script.py
+++ sd_training_project/my_first_script.py
@@ -3,5 +3,5 @@ import pandas as pd
 my_list = [1, 2, 3, 4, 5]
 my_list = [x + 1 for x in my_list]
 print("Now it's time to submit to GitHub")
-print("This is a new line")
 
+print ("FYI i remove last line")
