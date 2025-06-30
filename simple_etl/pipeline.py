0c6a13b4c4ace38992b6c3f2b8d0acb5b645e4ab
Stéphane Dresse <sdresse@bluesquarehub.com>
2025-06-30 16:34:20 +0200

    pipelineV2

--- simple_etl/pipeline.py
+++ simple_etl/pipeline.py
@@ -1,33 +1,54 @@
-from openhexa.sdk import pipeline, current_run
+from time import sleep
+import pandas as pd
 
-@pipeline(
-    "Calculatrice Pipeline",
-    inputs={
-        "input_1": {"type": "number", "min": 1, "max": 10, "required": True},
-        "input_2": {"type": "number", "min": 1, "max": 10, "required": True},
-        "operation": {"type": "choice", "choices": ["addition", "soustraction", "multiplication"], "required": True},
-    },
-)
-def calculatrice_pipeline(input_1: int, input_2: int, operation: str):
-    result = calcul(input_1, input_2, operation)
-    afficher_resultat(result)
+from openhexa.sdk import current_run, pipeline
 
-@calculatrice_pipeline.task
-def calcul(a: int, b: int, op: str) -> float:
-    if op == "addition":
-        res = a + b
-    elif op == "soustraction":
-        res = a - b
-    elif op == "multiplication":
-        res = a * b
-    else:
-        raise ValueError(f"Opération inconnue : {op}")
-    current_run.log_info(f"Résultat de {op} entre {a} et {b} = {res}")
-    return res
 
-@calculatrice_pipeline.task
-def afficher_resultat(result: float):
-    current_run.log_info(f"Le résultat final est : {result}")
+@pipeline("simple-etl")
+def simple_etl():
+    people_data = extract_people_data()
+    activity_data = extract_activity_data()
+    transformed_data = transform(people_data, activity_data)
+    load(transformed_data)
+
+
+@simple_etl.task
+def extract_people_data():
+    current_run.log_info("Extracting people data...")
+    sleep(2)  # Let's pretend we are querying an external system
+
+    return pd.DataFrame([{"id": 1, "first_name": "Mary", "last_name": "Johnson"},
+                         {"id": 2, "first_name": "Peter", "last_name": "Jackson"},
+                         {"id": 3, "first_name": "Taylor", "last_name": "Smith"}]).set_index("id")
+
+
+@simple_etl.task
+def extract_activity_data():
+    current_run.log_info(f"Extracting activity data...")
+    sleep(4)  # Let's pretend we are querying an external system
+
+    return pd.DataFrame([{"id": 1, "person": 1, "activity": "Activity 1"},
+                         {"id": 1, "person": 1, "activity": "Activity 1"},
+                         {"id": 1, "person": 1, "activity": "Activity 2"},
+                         {"id": 1, "person": 1, "activity": "Activity 3"},
+                         {"id": 1, "person": 2, "activity": "Activity 2"},
+                         {"id": 2, "person": 2, "activity": "Activity 3"},
+                         {"id": 2, "person": 3, "activity": "Activity 1"},
+                         {"id": 2, "person": 3, "activity": "Activity 2"}]).set_index("id")
+
+
+@simple_etl.task
+def transform(people_data, activity_data):
+    current_run.log_info(f"Transforming data...")
+    combined_df = activity_data.join(people_data, on="person").reset_index()
+
+    return combined_df
+
+
+@simple_etl.task
+def load(transformed_data):
+    current_run.log_info(f"Loading data ({len(transformed_data)} records)")
+
 
 if __name__ == "__main__":
-    calculatrice_pipeline()
+    simple_etl()
