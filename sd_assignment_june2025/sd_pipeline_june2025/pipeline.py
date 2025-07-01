e53a8d888a1cb67d008fca6c03d21493230b27d7
Stéphane Dresse <sdresse@bluesquarehub.com>
2025-07-01 11:06:43 +0200

    pushing changes to branch 'stephane'

---
+++ sd_assignment_june2025/sd_pipeline_june2025/pipeline.py
@@ -0,0 +1,31 @@
+"""Template for newly generated pipelines."""
+
+from openhexa.sdk import current_run, pipeline
+
+
+@pipeline("SD_pipeline_june2025")
+def sd_pipeline_june2025():
+    """Write your pipeline orchestration here.
+
+    Pipeline functions should only call tasks and should never perform IO operations or expensive computations.
+    """
+    count = task_1()
+    task_2(count)
+
+
+@sd_pipeline_june2025.task
+def task_1():
+    """Put some data processing code here."""
+    current_run.log_info("In task 1...")
+
+    return 42
+
+
+@sd_pipeline_june2025.task
+def task_2(count):
+    """Put some data processing code here."""
+    current_run.log_info(f"In task 2... count is {count}")
+
+
+if __name__ == "__main__":
+    sd_pipeline_june2025()
